52aa402c9cb777b3b00d70d84860dc1cd0aa540d
Daniel Miller <dmiller15@uchicago.edu>
2018-05-23 10:29:29 -0500

    feat(query): write to file; file cleanup

--- db_query.py
+++ db_query.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
+import argparse
 import MySQLdb
 import sys
+
+parser = argparse.ArgumentParser(description="Generate row swap file.")
+parser.add_argument('-o', help='Ouput Path')
+args = parser.parse_args()
 
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
                      passwd="",  # your password
                      db="hg38")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor()
+
+if args.o:
+    sys.stdout = open(args.o, 'w')
 
 # Queries
 queryHugo = "select display_label from xref where xref_id in (select display_xref_id from gene where stable_id = %(ensembl_id)s)"
@@ -17,21 +23,14 @@ queryBases = "select seq_region_start,seq_region_end from gene where stable_id =
 queryChrom = "select name from seq_region where seq_region_id in (select seq_region_id from gene where stable_id = %(ensembl_id)s)"
 
 for line in sys.stdin:
-    value = line.strip()
-    cur.execute(queryHugo, {'ensembl_id':value})
+    ensembl = line.strip()
+    # going to need an escape for the lines starting with __
+    cur.execute(queryHugo, {'ensembl_id':ensembl})
     hugo = cur.fetchone()[0]
-    cur.execute(queryBases, {'ensembl_id':value})
-    baseStart = cur.fetchone()[0]
-    baseEnd = cur.fetchone()[1]
-    cur.execute(queryChrom, {'ensembl_id':value})
+    cur.execute(queryBases, {'ensembl_id':ensembl})
+    baseStart,baseEnd = cur.fetchone()
+    cur.execute(queryChrom, {'ensembl_id':ensembl})
     chrom = cur.fetchone()[0]
-    print("%s\t%s\t%s\t%s" % (hugo,chrom,baseStart,baseEnd))
-
-# Use all the SQL you like
-#cur.execute("SELECT * FROM gene")
-
-# print all the first cell of all the rows
-#for row in cur.fetchall():
-#    print row[0]
+    print("%s\t%s\t%s\t%s\t%s" % (ensembl,hugo,chrom,baseStart,baseEnd))
 
 db.close()
