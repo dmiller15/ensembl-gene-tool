337a5e45b188f532534f85e65f38cb1d7d5d437f
Daniel Miller <dmiller15@uchicago.edu>
2018-05-24 13:26:11 -0500

    feat(query): escape for non-Ensembl lines

--- db_query.py
+++ db_query.py
@@ -19,18 +19,22 @@ if args.o:
 
 # Queries
 queryHugo = "select display_label from xref where xref_id in (select display_xref_id from gene where stable_id = %(ensembl_id)s)"
-queryBases = "select seq_region_start,seq_region_end from gene where stable_id = %(ensembl_id)s)"
+queryBases = "select seq_region_start,seq_region_end from gene where stable_id = %(ensembl_id)s"
 queryChrom = "select name from seq_region where seq_region_id in (select seq_region_id from gene where stable_id = %(ensembl_id)s)"
+
+print("%s\t%s\t%s\t%s\t%s" % ("Ensembl_Gene_ID", "HUGO_Gene_Symbol", "Chromosome", "Base_Start", "Base_End"))
 
 for line in sys.stdin:
     ensembl = line.strip()
-    # going to need an escape for the lines starting with __
-    cur.execute(queryHugo, {'ensembl_id':ensembl})
-    hugo = cur.fetchone()[0]
-    cur.execute(queryBases, {'ensembl_id':ensembl})
-    baseStart,baseEnd = cur.fetchone()
-    cur.execute(queryChrom, {'ensembl_id':ensembl})
-    chrom = cur.fetchone()[0]
-    print("%s\t%s\t%s\t%s\t%s" % (ensembl,hugo,chrom,baseStart,baseEnd))
+    if(ensembl.startswith("ENSG")):
+        cur.execute(queryHugo, {'ensembl_id':ensembl})
+        hugo = cur.fetchone()[0]
+        cur.execute(queryBases, {'ensembl_id':ensembl})
+        baseStart,baseEnd = cur.fetchone()
+        cur.execute(queryChrom, {'ensembl_id':ensembl})
+        chrom = cur.fetchone()[0]
+        print("%s\t%s\t%s\t%s\t%s" % (ensembl,hugo,chrom,baseStart,baseEnd))
+    else:
+        print("%s\t%s\t%s\t%s\t%s" % (ensembl,ensembl,ensembl,ensembl,ensembl))
 
 db.close()
